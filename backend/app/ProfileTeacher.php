1743288395b1a370f370257f421fcfe82970f67b
Gustavo <gustavog578@gmail.com>
2018-11-25 20:45:16 -0300

    fix model

--- backend/app/ProfileTeacher.php
+++ backend/app/ProfileTeacher.php
@@ -8,7 +8,7 @@ class ProfileTeacher extends Model
 {
     protected $table = 'profile_teacher';
 
-    protected $fillable = ['image','bio','link_presentation','native_language_id','country_id','likes','dislikes','presencial_mode','online_mode','contact_online','phone', 'public_email','latitud','longitud',]
+    protected $fillable = ['image','bio','link_presentation','native_language_id','country_id','likes','dislikes','presencial_mode','online_mode','contact_online','phone', 'public_email','latitud','longitud'];
 
     
 }
